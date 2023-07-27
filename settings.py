b578c8903b620cb990171d6027c1f0116821247a
Борис Бескровный <beskrovniibv@yandex.ru>
2023-07-27 14:58:20 +0500

    added all necessary congif in settings.py; added working with .env

--- settings.py
+++ settings.py
@@ -1,2 +1,7 @@
+import os
+
+
 class Config(object):
-    pass
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'sqlite:///db.sqlite3')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv('SECRET_KEY', 'DEFAULT_SECRET')
