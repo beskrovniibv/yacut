8b7e22badd7f8aab73214b22f8ac18bc15177f63
Борис Бескровный <beskrovniibv@yandex.ru>
2023-07-27 16:52:09 +0500

    fix index_view at yacut/views.py

--- yacut/views.py
+++ yacut/views.py
@@ -1,3 +1,5 @@
+from flask import render_template
+
 from . import app
 from .forms import URLMapForm
 from .models import URLMap
@@ -6,5 +8,5 @@ from .models import URLMap
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
-    
+    return render_template('index.html', form=form)
     pass
