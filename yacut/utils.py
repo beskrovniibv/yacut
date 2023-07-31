256ddd0e13ee8e137be64585e3e2b0aab3f55364
Борис Бескровный <beskrovniibv@yandex.ru>
2023-07-31 23:28:38 +0500

    made form URLMapForm worked

--- yacut/utils.py
+++ yacut/utils.py
@@ -6,9 +6,10 @@ from settings import ALLOWED_SYMBOLS
 
 
 def get_unique_short_id(url):
-    short = URLMap.query.get(original=url)
+    short = URLMap.query.filter_by(original=url).first()
     if short is None:
-        while short is None or URLMap.query.get(short=short).count != 0:
+        while short is None or URLMap.query.filter_by(short=short).count() != 0:
+            short = ''
             for _ in range(6):
                 short += choice(ALLOWED_SYMBOLS)
         return short, 201
--- yacut/views.py
+++ yacut/views.py
@@ -3,10 +3,14 @@ from flask import render_template
 from . import app
 from .forms import URLMapForm
 from .models import URLMap
+from .utils import get_unique_short_id
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index_view():
     form = URLMapForm()
+    if form.validate_on_submit():
+        url = form.original.data
+        return get_unique_short_id(url)
     return render_template('index.html', form=form)
     pass
