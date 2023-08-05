1f551b3de3a6d2e104db49dbb75b1ab13129b1ce
Борис Бескровный <beskrovniibv@yandex.ru>
2023-08-05 15:44:26 +0500

    fixed PEP issue

--- settings.py
+++ settings.py
@@ -10,6 +10,6 @@ SHORT_URL_PATTERN = re.compile(r'^[{allowed}]{{1,{max_len}}}$'.format(allowed=AL
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI ='sqlite:///db.sqlite3'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite3'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', default='SECRET_KEY')
