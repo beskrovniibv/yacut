40423267ff5720046b72d11a98b53a1afb07a4f3
Борис Бескровный <beskrovniibv@yandex.ru>
2023-08-05 15:27:44 +0500

    hardcoded flask app name at yacut/__init__.py

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 from settings import Config
 
-app = Flask(__name__)
+app = Flask('yacut')
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
