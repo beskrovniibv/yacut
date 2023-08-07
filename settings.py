1983421eaf8828c7f291d617e62d5d815f2571f5
Борис Бескровный <beskrovniibv@yandex.ru>
2023-08-07 15:55:40 +0500

    modify class Congif at /settings.py according to the model from lessons

--- settings.py
+++ settings.py
@@ -10,6 +10,6 @@ SHORT_URL_PATTERN = re.compile(r'^[{allowed}]{{1,{max_len}}}$'.format(allowed=AL
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URI", "sqlite:///db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', default='sqlite:///db.sqlite3')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv("SECRET_KEY", "SECRET_KEY")
+    SECRET_KEY = os.getenv('SECRET_KEY', default='MY SECRET KEY is most secure')
