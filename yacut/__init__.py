bcf8c4d4770bc9dc45e1f2738d5d49aadc0bfb22
Борис Бескровный <beskrovniibv@yandex.ru>
2023-08-07 15:52:41 +0500

    modify yacut/__init__.py according to the model from lessons

--- yacut/__init__.py
+++ yacut/__init__.py
@@ -4,9 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 
 from settings import Config
 
-app = Flask('yacut')
+app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-# migrate = Migrate(app, db)
+migrate = Migrate(app, db)
 
-from . import api_views, views
+from . import api_views, error_handlers, views
